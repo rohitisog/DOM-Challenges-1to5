052ac5c6af5e94ad3541ba641d627fb10808cdb9
rohitisog <rohitisog@gmail.com>
2025-02-26 14:06:47 +0530

    completed challenge 3

--- challenge-3/app.js
+++ challenge-3/app.js
@@ -1,3 +1,31 @@
 /**
  * Write your challenge solution here
  */
+
+const nameInput = document.getElementById("nameInput");
+const jobInput = document.getElementById("jobInput");
+const ageInput = document.getElementById("ageInput");
+const bioInput = document.getElementById("bioInput");
+
+const nameDisplay = document.getElementById("nameDisplay");
+const jobDisplay = document.getElementById("jobDisplay");
+const ageDisplay = document.getElementById("ageDisplay");
+const bioDisplay = document.getElementById("bioDisplay");
+
+function handleName() {
+  nameDisplay.textContent = nameInput.value;
+}
+function handleJob() {
+  jobDisplay.textContent = jobInput.value;
+}
+function handleAge() {
+  ageDisplay.textContent = ageInput.value;
+}
+function handleBio() {
+  bioDisplay.textContent = bioInput.value;
+}
+
+nameInput.addEventListener("input", handleName);
+jobInput.addEventListener("input", handleJob);
+ageInput.addEventListener("input", handleAge);
+bioInput.addEventListener("input", handleBio);
