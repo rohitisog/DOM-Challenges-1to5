f2a6b7ce3012c4603806dc038c23e58c17b90dd5
rohitisog <rohitisog@gmail.com>
2025-02-26 13:20:37 +0530

    Removed node_module folder

--- challenge-1/app.js
+++ challenge-1/app.js
@@ -1,3 +1,25 @@
 /**
  * Write your challenge solution here
  */
+
+const bulb = document.getElementById("bulb");
+const toggleButton = document.getElementById("toggleButton");
+const statu = document.getElementById("status");
+const body = document.getElementById("body");
+const isOn = true;
+
+toggleButton.addEventListener("click", function turnOnOff() {
+  if (isOn) {
+    bulb.classList.remove("off");
+    toggleButton.innerHTML = "Turn On";
+    statu.innerHTML = "Status: Off";
+    body.style.backgroundColor = "yellow";
+    isOn = false;
+  } else {
+    body.style.backgroundColor = "grey";
+    bulb.classList.add("off");
+    toggleButton.textContent = "Turn Off";
+    statu.textContent = "Status: On";
+    isOn = true;
+  }
+});
